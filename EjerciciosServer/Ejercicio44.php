84079da8fb25c4ed7027f10e971125a8e4b6f371
Mario <molinamario.msc@gmail.com>
2023-01-30 13:12:20 +0100

    Ej44 sin acabar (pdf)

--- EjerciciosServer/Ejercicio44.php
+++ EjerciciosServer/Ejercicio44.php
@@ -1,6 +1,92 @@
 <?
+/* ?><pre><?=print_r($x)?></pre><? */
+
 /* GENERAR PDF */
 if(isset($_POST['pdf'])){
+  include "../../includes/LoginMySql.php";
+  include("../includes/fpdf/PDF_WriteTag.php");
+  define('FPDF_FONTPATH',"../includes/fpdf/font");
+
+  //EXTENSIÓN
+  class fpdf2 extends FPDF {
+    function Header(){
+      $title = "LISTADO DE ALUMNOS MATRICULADOS";
+      $this->SetFont('Arial','B',15);
+      $w = $this->GetStringWidth($title);
+      $this->SetX((210-15*2-$w)/2+15);
+      $this->SetFillColor(255);
+      $this->SetTextColor(0);
+      $this->Cell($w,9,$title,0,1,'C',true);
+      $this->Ln(5);
+      $this->y0 = $this->GetY();
+    }
+
+    function Footer(){
+      $this->SetY(-15);
+      $this->SetFont('Arial','I',8);
+      $this->SetTextColor(128);
+      $this->Cell(0,10,utf8_decode('Página '.$this->PageNo().' de {nb}'),0,0,'C');
+    }
+
+    /* function SetCol(){
+      $x = 15;
+      $this->SetLeftMargin($x);
+      $this->SetX($x);
+    }
+
+    function AcceptPageBreak(){
+      $this->SetCol();
+      $this->SetY($this->y0);
+      return true;
+    } */
+  }
+
+  $pdf1=new fpdf2('P','mm','A4');
+  $pdf1->setdisplaymode('fullpage',"single");
+  $pdf1->AliasNbPages();
+  $pdf1->SetMargins(15,15);
+  $pdf1->Addpage();
+  $pdf1->SetTextColor(0);
+  $pdf1->SetFont("arial","B", 11);
+
+  //IMPRESIÓN
+  $c = new mysqli($host, $user, $pass, 'practicas') or die("No ha podido realizarse la conexión");
+  $res = $c->query("select * from alumnos;");
+  $encab = $res->fetch_fields();
+  $c->close();
+
+  $w=[30,68,30,15];
+  $top=30;
+  $h=25;
+  $pdf1->setxy(15,$top);
+
+  $pdf1->cell(40, 20, 'Foto');
+  $pdf1->setxy(15+$h+10,$top);
+
+  for($i=0; $i<4; $i++){
+    $pdf1->cell($w[$i], 20, utf8_decode($encab[$i]->name));
+  }
+
+  $pdf1->line(15,$top+15,210-15,$top+15);
+
+  $pdf1->SetFont("arial","", 11);
+  $top+=25;
+  $pdf1->setxy(15+$h+10,$top);
+
+  while($fila = $res->fetch_row()){
+    $pdf1->Image($fila[4],15,$top,$h);
+    array_pop($fila);
+    for($i=0; $i<4; $i++){
+      $pdf1->cell($w[$i], 20, utf8_decode($fila[$i]));
+    }
+    $top+=$h;
+    $pdf1->setxy(15+$h+10,$top);
+  }
+
+  //ENVÍO Y CIERRE
+  $pdf1->Output();
+
+  //-----------------------------------------------------------------------------------------------
 
 } else {
   /* AÑADIR A BBDD */
@@ -11,21 +97,20 @@ if(isset($_POST['pdf'])){
     $c = new mysqli($host, $user, $pass) or die("No ha podido realizarse la conexión");
     $rs=$c->query("show databases");
     $databases = $rs->fetch_all();
-    echo $databases;
-    if(!in_array("practicas", $databases)){
+    foreach($databases as $base) $bases[] = $base[0];
+    if(!in_array("practicas", $bases)){
       $c->query("create database Practicas");
       $c->query("use Practicas");
       $createQuery = "create table alumnos(dni varchar(9) primary key,nomApes varchar(50),fechaNac varchar(10),esRepet varchar(1) check (esRepet='S' or esRepet='N'),rutaFoto varchar(100));";
-      if($c->query($createQuery)) echo "<br>Tabla creada con éxito";
-      else echo "<br>Error al crear la tabla: ".$c->$error;
+      if(!$c->query($createQuery)) echo "<br>Error al crear la tabla: ".$c->$error;
     }
 
     //Almacenar valores
     $dni = $_POST['dni'];
     $nomApes = $_POST['nom'].' '.$_POST['apes'];
     $fnac = $_POST['fnac'];
-    $repet = ($_POST['fnac']=='on') ? 'S' : 'N';
-    $foto = 'C:/Apache24/htdocs/EjerciciosServer/Alumnos/'.$_FILES['foto']['name'];
+    $repet = ($_POST['repet']=='on') ? 'S' : 'N';
+    $foto = 'Alumnos/'.$_FILES['foto']['name'];
     move_uploaded_file($_FILES['foto']['tmp_name'],$foto);
 
     //Añadir nuevo registro
@@ -33,7 +118,10 @@ if(isset($_POST['pdf'])){
     $c->query("insert into alumnos values ('$dni','$nomApes','$fnac','$repet','$foto');");
     $c->close();
   }
+
+  //-----------------------------------------------------------------------------------------------
 ?>
+
 <!-- FORMULARIO HTML -->
 <!DOCTYPE html>
 <html lang="en">
@@ -48,12 +136,12 @@ if(isset($_POST['pdf'])){
   <div class="row justify-content-center">
     <div class="col-6">
 
-      <div class="row mb-5 mt-4">
-        <div class="col-6"><h2 class="">Nuevo Alumno</h2></div>
-        <div class="col-6 text-end"><button class="btn btn-primary" type="submit">Generar PDF</button></div>
-      </div>
-      
-      <form class="needs-validation" novalidate="" action="" method="post" enctype="multipart/form-data">
+      <form class="needs-validation mt-5" novalidate="" action="" method="post" enctype="multipart/form-data">
+        <div class="row mb-5">
+          <div class="col-6"><h2 class="">Nuevo Alumno</h2></div>
+          <div class="col-6 text-end"><input type="submit" class="btn btn-primary" name="pdf" value="Generar PDF"></div>
+        </div>
+
         <div class="row g-3">
           <div class="col-4">
             <label for="firstName" class="form-label">Nombre</label>
@@ -102,7 +190,11 @@ if(isset($_POST['pdf'])){
             </div>
           </div>
 
-        <button class="w-100 btn btn-primary btn-lg mt-5" type="submit" name="anadir" value="anadir">Añadir Alumno/a</button>
+          <div class="col-3"></div>
+          <div class="col-6">
+            <button class="w-100 btn btn-primary btn-lg mt-5" type="submit" name="anadir" value="anadir">Añadir Alumno/a</button>
+          </div>
+        
       </form>
 
     </div>
