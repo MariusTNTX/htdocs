be3857a94e7a22fac35aafcbce5b8768de1bef5b
MariusTNTX <molinamario.msc@gmail.com>
2023-02-18 11:07:29 +0100

    phpmyadmin no va en el nuevo

--- Biblioteca/php/CrearTabladesdeCSVDepartamentos.php
+++ Biblioteca/php/CrearTabladesdeCSVDepartamentos.php
@@ -1,18 +1,18 @@
 <?
 	$dbhost="localhost";
 	$dbuser="root";
-	$dbpass="1234";
+	$dbpass="admin";
 
 	$c1 = mysqli_connect($dbhost,$dbuser,$dbpass) or die ('Error de conexion a mysql: ' . mysqli_error($c1).'<br>');
-	echo utf8_encode('Conexión establecida').'<br>';
+	echo utf8_encode('ConexiÃ³n establecida').'<br>';
 	
-	if (!mysqli_query($c1,'create database if not exists BibliotecaAngelM')){
+	if (!mysqli_query($c1,'create database if not exists BibliotecaMolinaM')){
 		echo mysqli_error($c1).'<br>';
 		exit(-1);
 	}
 	echo utf8_encode('Base de datos creada').'<br>';
 
-	if (!mysqli_query($c1,'use BibliotecaAngelM')){
+	if (!mysqli_query($c1,'use BibliotecaMolinaM')){
 		echo mysqli_error($c1).'<br>';
 		exit(-1);
 	}
@@ -20,22 +20,25 @@
 
 
 	//Creamos la tabla destino de los datos
-	if (!mysqli_query($c1,'create table if not exists departamentos (COD_DPTO tinyint(2) primary key, NOMBRE varchar(40), CENTRO varchar(15), DNI_JFP varchar(9), PASSWORD varchar(65))')){
+	if (!mysqli_query($c1,"create table if not exists DEPARTAMENTOS (COD_DPTO VARCHAR(2) PRIMARY KEY, NOMBRE VARCHAR(50) NOT NULL, CENTRO ENUM('CIFP1','Albaladejito','Pedro Mercedes'), DNI_JFK VARCHAR(10), PASSWORD VARCHAR(100))")){
 		echo mysqli_error($c1).'<br>';
 		exit(-1);
 	}
 	echo utf8_encode('Tabla creada').'<br>';
 
 	//Importacion de datos desde un fichero a una tabla	
-	if (!mysqli_query($c1,"LOAD DATA LOCAL INFILE '" . $_SERVER['DOCUMENT_ROOT'] . "/Biblioteca/Departamentos.csv" . "'
-             into table departamentos
-             fields terminated by ';'
-             lines terminated by '\n'
-             ignore 1 rows")){
-		echo mysqli_error($c1).'<br>';
-		exit(-1);
-    }
-    echo utf8_encode('Importación realizada').'<br>';
+	$fic = fopen('../csv/Departamentos.csv','r');
+	fgetcsv($fic,0,';');
+	while($reg = fgetcsv($fic,0,';')){
+		$stmt = mysqli_prepare($c1,"insert into DEPARTAMENTOS values(?,?,?,?,?)");
+		$stmt->bind_param("issss", $reg[0], $reg[1], $reg[2], $reg[3], $reg[4]);
+		if (!$stmt->execute()){
+			echo 'error en insert: '.mysqli_error($c1).'<br>';
+			exit(-1);
+		}
+		$stmt->close();
+	}
+  echo utf8_encode('ImportaciÃ³n realizada').'<br>';
 	
 	//Liberamos gestor de conexion
 	mysqli_close($c1);
