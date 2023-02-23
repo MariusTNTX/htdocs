7c6f652da04725efdda0db8e0a68c50814afb9ee
Mario <molinamario.msc@gmail.com>
2023-02-23 19:39:09 +0100

    login alumnos

--- Biblioteca/js/index.js
+++ Biblioteca/js/index.js
@@ -91,50 +91,13 @@ async function getAlumnos(cod,ape,nom,dni,nie){
   values = values.join('|');
 
   //Se obtiene el JSON de resultados:
-  console.log(`http://192.168.2.30/Biblioteca/php/api.php?select=libros&filters=${filters}&values=${values}`)
-  let response = await fetch(`http://localhost/Biblioteca/php/api.php?select=alumnos&filters=${filters}&values=${values}`,{
+  console.log(`http://192.168.2.30/Biblioteca/php/api.php?select=alumnos&filters=${filters}&values=${values}`)
+  let request = await fetch(`http://localhost/Biblioteca/php/api.php?select=alumnos&filters=${filters}&values=${values}`,{
     method: 'GET',
     headers: {'Content-Type': 'application/json'}
   });
-  response = await response.json();
-  console.log(response);
-
-  //Se imprimen los resultados en la tabla
-  /* tbody.innerHTML = "";
-  for(let res of response){
-    let disabled = (res.USUARIO=='CONSULTA') ? 'disabled' : '';
-    tbody.innerHTML += `
-    <tr class="align-middle">
-      <td>${res.TITULO}</td>
-      <td>${res.COD_LIBRO}</td>
-      <td>${res.AUTOR}</td>
-      <td>${res.MATERIA}</td>
-      <td>${res.NOMBRE}</td>
-      <td>${res.CENTRO}</td>
-      <td>${res.EDITORIAL}</td>
-      <td>${res.A_EDICION}</td>
-      <td>${res.ESTADO}</td>
-      <td>${res.USUARIO}</td>
-      <td><button type="button" class="btn btn-primary btn-Reservar" data-bs-toggle="modal" ${disabled} data-bs-target="#modalLogin">Reservar</button></td>
-    </tr>`;
-  } */
-
-  //Se personaliza el login del modal según el usuario
-  /* for(let btn of document.querySelectorAll(".btn-Reservar")){
-    btn.addEventListener("click",(e)=>{
-      let usuario = e.currentTarget.parentElement.previousElementSibling.textContent;
-      switch(usuario){
-        case 'ALUMNO': 
-          loginAlumnos.classList.remove("d-none");
-          loginProfesores.classList.add("d-none");
-          break;
-        case 'PROFESOR': 
-          loginAlumnos.classList.add("d-none");
-          loginProfesores.classList.remove("d-none");
-          break;
-      }
-    });
-  } */
+  request = await request.json();
+  return Promise.resolve(request);
 }
 
 // BOTÓN BUSCAR
@@ -145,13 +108,36 @@ buscarLibros.addEventListener("click",()=>{
 // BOTONES LOGIN
 for(let btn of document.querySelectorAll(".btn-login")){
   btn.addEventListener("click",(e)=>{
+    //Se previene el submit
+    e.preventDefault();
+    //Se obtiene un array con los imputs a rellenar según sea alumno o prefesor
     let campos = e.target.parentElement.parentElement.querySelectorAll(".campoLogin");
-    if(campos.length==2){ //Alumnos
-      //getAlumnos con dni y nie
-      getAlumnos('','','',campos[0].value,campos[1].value);
-    } else if(campos.length==1){ //Profesores
-      //getProfesores con dni
+    //Login de Alumno:
+    if(campos.length==2){
+      getAlumnos('','','',campos[0].value,campos[1].value)
+      .then(data => {
+        if(data.length==1){
+          //Se meten los datos en la sesión (pendiente)
+          console.log(data);
+          //Se produce un click en el hidden para continuar
+          e.target.parentElement.nextElementSibling.dispatchEvent(new Event("click"));
+        } else alert("Credenciales Incorrectas");
+      });
     }
+    //Login de Profesor:
+    else if(campos.length==1){
+      getProfesores('','',campos[0].value,'')
+      .then(data => {
+        if(data.length==1){
+          //Se meten los datos en la sesión (pendiente)
+          console.log(data);
+          //Se produce un click en el hidden para continuar
+          e.target.parentElement.nextElementSibling.dispatchEvent(new Event("click"));
+        } 
+        else alert("Credenciales Incorrectas");
+      });
+    } 
+    
     //Si no se encuentran resultados se previene el evento y se indica error
-  })
+  });
 }
--- Biblioteca/php/api.php
+++ Biblioteca/php/api.php
@@ -10,6 +10,8 @@ if(isset($_GET['select'])){
   $select = $_GET['select'];
   $filters = explode("|", $_GET['filters']);
   $values = explode("|", $_GET['values']);
+  //CAMPOS QUE SE BUSCARÁN SIEMPRE CON "="
+  $igual = ['alumno','dni','nie','cod_dpto','dni_jfk','a_edicion','soporte_m','usuario','estado'];
   
   //LIBROS
   if($select=='libros'){
@@ -46,7 +48,8 @@ if(isset($_GET['select'])){
     foreach($filters as $i => $filt){
       if($i==0) $consulta .= ' WHERE ';
       else $consulta .= ' AND ';
-      if(strlen($filt)>0) $consulta .= $filt.' LIKE "%'.$values[$i].'%"';
+      if(in_array($filt,$igual)) $consulta .= $filt.' = "'.$values[$i].'"';
+      else $consulta .= $filt.' LIKE "%'.$values[$i].'%"';
     }
     //Se establece conexión con la BD
     $c1 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die ('Error de conexion a mysql: ' . mysqli_error($c1).'<br>');
@@ -58,11 +61,27 @@ if(isset($_GET['select'])){
     }
     //Se almacenan los datos
     $data = mysqli_fetch_all($resp, MYSQLI_ASSOC);
-    //Correcciones sobre el array final: Corregir ISBN y Quitar Cod_DPTO
-    /* foreach($data as $i => $d){
-      $data[$i]['COD_LIBRO'] = substr($data[$i]['COD_LIBRO'], 0, strlen($data[$i]['COD_LIBRO'])-2);
-      array_pop($data[$i]);
-    } */
+  }
+
+  //PROFESORES
+  else if($select=='profesores'){
+    //Se forma la raíz de la consulta
+    $consulta = 'SELECT APELLIDOS, P.NOMBRE, DNI, D.NOMBRE FROM PROFESORES P, DEPARTAMENTOS D WHERE P.COD_DPTO=D.COD_DPTO';
+    //Se completa la consulta con los filtros corregidos
+    foreach($filters as $i => $filt){
+      if(in_array($filt,$igual)) $consulta .= ' AND '.$filt.' = "'.$values[$i].'"';
+      else $consulta .= ' AND '.$filt.' LIKE "%'.$values[$i].'%"';
+    }
+    //Se establece conexión con la BD
+    $c1 = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die ('Error de conexion a mysql: ' . mysqli_error($c1).'<br>');
+    //Se realiza la consulta
+    if (!$resp = mysqli_query($c1, $consulta)){
+      echo mysqli_error($c1).'<br>';
+      echo 'Consulta: '.$consulta;
+      exit(-1);
+    }
+    //Se almacenan los datos
+    $data = mysqli_fetch_all($resp, MYSQLI_ASSOC);
   }
   
   //DEVOLUCIÓN DE JSON
