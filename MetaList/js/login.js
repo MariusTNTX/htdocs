1d125ec18ec2d286c703e59cc34ace3997621d04
MSIMario <mmolina@serbatic.es>
2023-04-30 19:54:14 +0200

    logout y login persistente

--- MetaList/js/login.js
+++ MetaList/js/login.js
@@ -1,15 +1,96 @@
-let loginEmail = document.getElementById("loginEmail");
-let loginPass = document.getElementById("loginPass");
-let loginButton = document.getElementById("loginButton");
-let loginPassEye = document.getElementById("loginPassEye");
+function addShowPassSwitch(loginPassEye, loginPass){
+  console.log("addshowpassswitch")
+  console.log(loginPassEye)
+  console.log(loginPass)
+  loginPassEye.addEventListener("click",()=>{
+    if(/* passView */ loginPassEye.classList.contains("bi-eye-slash-fill")){
+      loginPass.type = "password";
+      loginPassEye.classList.replace("bi-eye-slash-fill","bi-eye-fill");
+    } else {
+      loginPass.type = "text";
+      loginPassEye.classList.replace("bi-eye-fill","bi-eye-slash-fill");
+    }
+  });
+}
 
-//Mostrar Contraseña
-addShowPassSwitch(loginPassEye,loginPass);
-
-//BOTÓN LOGIN
-loginButton.addEventListener("click",(e)=>{
-  e.preventDefault();
-  if(loginEmail.value.length>0 && loginPass.value.length>0){
-    
+function showUserOptions(show){
+  if(show){
+    document.getElementById("dropdownLogin").innerHTML = userOptions;
+    document.getElementById("userName").textContent = sessionStorage.getItem("usuario");
+    if(sessionStorage.getItem("foto")=="null") document.getElementById("userImgMini").src = "./imagenes/basico/user_MetaList.png";
+    else document.getElementById("userImgMini").src = sessionStorage.getItem("foto");
+    addLogoutEvent(document.getElementById("logOut"));
+  } else {
+    document.getElementById("dropdownLogin").innerHTML = loginForm;
+    addLoginEvent(document.getElementById("loginButton"));
   }
-});
+}
+
+async function login(email="", pass=""){
+  console.log("Email="+email+", Pass="+pass);
+  if(email.length>0 && pass.length>0){
+    let usuarios = await list("usuarios",true,["emailUsuario",email]);
+    console.log(usuarios);
+    if(usuarios.response.length==1){
+      let passVerif = await checkPassword(email, pass, true);
+      console.log(passVerif);
+      if(passVerif[0].coincidence){
+        if(passVerif[0].verify){
+          let usuario = usuarios.response[0];
+          //Meter datos del usuario en sesión
+          sessionStorage.setItem("email",usuario.email);
+          sessionStorage.setItem("usuario",usuario.nombre);
+          sessionStorage.setItem("foto",usuario.foto);
+          sessionStorage.setItem("permisos",usuario.permisos);
+          sessionStorage.setItem("notificaciones",usuario.notificaciones);
+          sessionStorage.setItem("fecha",usuario.fecha);
+          //Cambiar Opciones del Login
+          showUserOptions(true);
+          //Mensaje de éxito
+          showAlert("SUCCESS","Bienvenido/a "+sessionStorage.getItem("usuario"));
+        } else showAlert("ERROR","La contraseña indicada es incorrecta. Inténtalo de nuevo");
+      } else showAlert("ERROR","No existe ningún usuario con la dirección de correo electrónico especificada");
+    } else showAlert("ERROR","No existe ningún usuario con la dirección de correo electrónico especificada");
+  } else if(email.length==0 && pass.length==0) showAlert("ERROR","Debes indicar tu dirección de correo electrónico y tu contraseña");
+  else if(email.length==0) showAlert("ERROR","Debes indicar tu dirección de correo electrónico");
+  else if(pass.length==0) showAlert("ERROR","Debes indicar tu contraseña");
+}
+
+function logout(){
+  console.log("logout");
+  sessionStorage.removeItem("email");
+  sessionStorage.removeItem("usuario");
+  sessionStorage.removeItem("foto");
+  sessionStorage.removeItem("permisos");
+  sessionStorage.removeItem("notificaciones");
+  sessionStorage.removeItem("fecha");
+  showUserOptions(false);
+  showAlert("INFO","Se ha cerrado la sesión");
+}
+
+function addLoginEvent(loginButton){
+  console.log("login")
+  console.log(loginButton)
+  if(loginButton){
+    console.log("login in")
+    addShowPassSwitch(document.getElementById("loginPassEye"),document.getElementById("loginPass"));
+    loginButton.addEventListener("click",(e)=>{
+      e.preventDefault();
+      login(document.getElementById("loginEmail").value,document.getElementById("loginPass").value);
+    });
+  } 
+}
+
+function addLogoutEvent(logoutButton){
+  console.log("LogOut Event");
+  console.log(logoutButton);
+  if(logoutButton) logoutButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    logout();
+  });
+}
+
+//Si hay un usuario guardado en sesión se muestran las opciones del usuario
+if(sessionStorage.getItem("email")) showUserOptions(true);
+//Sino se añade el evento del botón login
+else addLoginEvent(document.getElementById("loginButton"));
--- MetaList/js/util.js
+++ MetaList/js/util.js
@@ -37,21 +37,9 @@ let userOptions = `
     <li><a class="dropdown-item" href="favoritos.html"><i class="bi bi-heart-fill me-2"></i>Favoritos</a></li>
     <li><a class="dropdown-item" href="recomendaciones.html"><i class="bi bi-star-fill me-2"></i>Recomendaciones</a></li>
     <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#"><i class="bi bi-door-closed-fill me-2"></i>Cerrar Sesión</a></li>
+    <li><a class="dropdown-item" href="#" id="logOut"><i class="bi bi-door-closed-fill me-2"></i>Cerrar Sesión</a></li>
   </ul>
 `;
-
-function addShowPassSwitch(loginPassEye, loginPass){
-  loginPassEye.addEventListener("click",()=>{
-    if(/* passView */ loginPassEye.classList.contains("bi-eye-slash-fill")){
-      loginPass.type = "password";
-      loginPassEye.classList.replace("bi-eye-slash-fill","bi-eye-fill");
-    } else {
-      loginPass.type = "text";
-      loginPassEye.classList.replace("bi-eye-fill","bi-eye-slash-fill");
-    }
-  });
-}
 
 function showAlert(tipo,txt){
   tipo = tipo.trim().toLowerCase();
@@ -76,37 +64,3 @@ function showAlert(tipo,txt){
   document.getElementById("alertStart").dispatchEvent(new Event("click"));
 }
 
-async function login(email="", pass=""){
-  console.log("Email="+email+", Pass="+pass);
-  if(email.length>0 && pass.length>0){
-    let usuarios = await list("usuarios",true,["emailUsuario",email]);
-    console.log(usuarios);
-    if(usuarios.response.length==1){
-      let passVerif = await checkPassword(email, pass, true);
-      console.log(passVerif);
-      if(passVerif[0].coincidence){
-        if(passVerif[0].verify){
-          let usuario = usuarios.response[0];
-          //Meter datos del usuario en sesión
-          sessionStorage.setItem("email",usuario.email);
-          sessionStorage.setItem("usuario",usuario.nombre);
-          sessionStorage.setItem("foto",usuario.foto);
-          sessionStorage.setItem("permisos",usuario.permisos);
-          sessionStorage.setItem("notificaciones",usuario.notificaciones);
-          sessionStorage.setItem("fecha",usuario.fecha);
-          //Cambiar Opciones del Login
-          document.getElementById("dropdownLogin").innerHTML = userOptions;
-          document.getElementById("userName").textContent = sessionStorage.getItem("usuario");
-          if(sessionStorage.getItem("foto")=="null") document.getElementById("userImgMini").src = "./imagenes/basico/user_MetaList.png";
-          else document.getElementById("userImgMini").src = sessionStorage.getItem("foto");
-          //Mensaje de éxito
-          showAlert("SUCCESS","Bienvenido/a "+sessionStorage.getItem("usuario"));
-        } else showAlert("ERROR","La contraseña indicada es incorrecta. Inténtalo de nuevo");
-      } else showAlert("ERROR","No existe ningún usuario con la dirección de correo electrónico especificada");
-    } else showAlert("ERROR","No existe ningún usuario con la dirección de correo electrónico especificada");
-  } else if(email.length==0 && pass.length==0) showAlert("ERROR","Debes indicar tu dirección de correo electrónico y tu contraseña");
-  else if(email.length==0) showAlert("ERROR","Debes indicar tu dirección de correo electrónico");
-  else if(pass.length==0) showAlert("ERROR","Debes indicar tu contraseña");
-}
-
-document.getElementById("loginButton").addEventListener("click",()=>login(document.getElementById("loginEmail").value,document.getElementById("loginPass").value));
