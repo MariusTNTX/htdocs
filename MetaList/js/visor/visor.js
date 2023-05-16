d97894a86ba503a228fd5bae0ca01ad7f4787421
MSIMario <mmolina@serbatic.es>
2023-05-16 10:14:20 +0200

    primera propuesta util de visor

--- MetaList/js/visor/visor.js
+++ MetaList/js/visor/visor.js
@@ -1,7 +1,19 @@
+// Gestión de Likes (trasladar a función head)
+let heart1 = document.getElementById("heart1");
+let heart2 = document.getElementById("heart2");
+heart1.addEventListener("click",()=>{
+    heart1.classList.add("hid");
+    heart2.classList.remove("hid");
+});
+heart2.addEventListener("click",()=>{
+    heart1.classList.remove("hid");
+    heart2.classList.add("hid");
+});
+
 let etiquetas = {
   head: {title, subtitle, link, heart},
   image: "",
-  element: [{element, content, link}],
+  mainInfo: [{element, content, link}],
   recording: {labels: [{content, link}], studios: [{content, link}], roles: [{content, link}]},
   bandStages: [{inicYear, finYear, band}],
   lyricThemes: [{theme}],
@@ -15,20 +27,6 @@ let etiquetas = {
   }
 };
 
-let metadata = {
-  //ARRAY 1: PETICIONES NECESARIAS POR CADA ELEMENTO
-  //ARRAY 2: ETIQUETAS A IMPRIMIR POR CADA ELEMENTO (FUNCIONES)
-  album: {},
-  band: {},
-  label: {},
-  musician: {}
-};
-
-//OBTENCIÓN DE TODOS LOS DATOS NECESARIOS DE LA API (array 1)
-
-//RECORRIDO DE ETIQUETAS: IMPRESIÓN HTML (array 2)
-  //Se necesita saber donde buscar en el array data
-
 let params = getURLParameters();
 
 switch(params.element){
@@ -40,4 +38,72 @@ switch(params.element){
     break;
   case 'musician': console.log("Es un músico :D")
     break;
+}
+
+let metadata = {
+  //ARRAY 1: PETICIONES NECESARIAS POR CADA ELEMENTO
+  //ARRAY 2: ETIQUETAS A IMPRIMIR POR CADA ELEMENTO (FUNCIONES)
+  album: {
+    requests: {
+      elements: ['albumes_plus', 'discograficas_albumes', 'estudios_albumes', 'generos_albumes', 'roles_musicos_albumes', 'canciones_albumes'],
+      params: [['nombreAlbum','params.album'],['nombreBanda','params.band']],
+    },
+    cards: {
+      element: 'album',
+      functions: [head, image, mainInfo, recording, topGenres, topMusicians, topSongs, description, links]
+    }
+  },
+  band: {},
+  label: {},
+  musician: {}
+};
+
+//OBTENCIÓN DE TODOS LOS DATOS NECESARIOS DE LA API (array 1)
+
+//RECORRIDO DE ETIQUETAS: IMPRESIÓN HTML (array 2)
+  //Se necesita saber donde buscar en el array data
+
+
+
+function head(elm, data){
+  
+}
+function image(elm, data){
+
+}
+function element(elm, data){
+
+}
+function recording(elm, data){
+
+}
+function bandStages(elm, data){
+
+}
+function lyricThemes(elm, data){
+
+}
+function description(elm, data){
+
+}
+function links(elm, data){
+
+}
+function topAlbums(elm, data){
+
+}
+function topBands(elm, data){
+
+}
+function topLabels(elm, data){
+
+}
+function topMusicians(elm, data){
+
+}
+function topSongs(elm, data){
+
+}
+function topGenres(elm, data){
+
 }
