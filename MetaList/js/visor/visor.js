c88f8bd0f04563a37cb95257798fbff0f16dd4ac
MSIMario <mmolina@serbatic.es>
2023-05-18 18:41:35 +0200

    todos los visores mas o menos

--- MetaList/js/visor/visor.js
+++ MetaList/js/visor/visor.js
@@ -57,7 +57,15 @@ let metadata = {
     ],
     cards: [head, image, mainInfo, recording, topGenres, topMusicians, topSongs, topAlbums, description, links]
   },
-  label: {},
+  label: {
+    requests: [
+      {element: ['discograficas'], params: [['nombreDiscografica','params.label']]},
+      {element: ['albumes'], params: [['nombreDiscografica','params.label'],['order','anioAlbum']]},
+      {element: ['bandas'], params: [['nombreDiscografica','params.label']]},
+      {element: ['canciones_albumes'], params: [['nombreDiscografica','params.label'],['order','estrellasCancion_Desc'],['page','1'],['limit','10']]},
+    ],
+    cards: [head, image, mainInfo, recording, topMusicians, topSongs, topAlbums, topBands, links]
+  },
   musician: {
     requests: [
       {element: ['musicos'], params: [['nombreMusico','params.musician']]},
@@ -65,8 +73,8 @@ let metadata = {
       {element: ['roles_musicos_albumes'], params: [['nombreMusico','params.musician']]},
       {element: ['canciones_albumes'], params: [['nombreMusicoEtapa','params.musician'],['order','estrellasCancion_Desc']]},
       {element: ['albumes'], params: [['nombreMusico','params.musician'],['order','anioAlbum']]},
-      {element: ['bandas'], params: [['nombreMusicoEtapa','params.musician'],['order','anioInicioEtapaMusico']]},
-      {element: ['canciones_albumes'], params: [['nombreMusicoEtapa','params.musician'],['order','estrellasCancion_Desc'],['page','1'],['limit','10']]},
+      {element: ['bandas'], params: [['nombreMusicoEtapa','params.musician']]},
+      {element: ['canciones_albumes'], params: [['nombreMusico','params.musician'],['order','estrellasCancion_Desc'],['page','1'],['limit','10']]},
     ],
     cards: [head, image, mainInfo, recording, topSongs, topAlbums, topBands, links]
   }
@@ -101,7 +109,7 @@ function head(elm, data){
     headElm.innerHTML = `<h1>${data.bandas[0].banda}</h1>`;
     heartElm.classList.remove("d-none");
   } else if(elm=='label'){
-
+    headElm.innerHTML = `<h1>${data.discograficas[0].discografica}</h1>`;
   } else if(elm=='musician'){
     headElm.innerHTML = `<h1>${data.musicos[0].musico}</h1>`;
   }
@@ -116,7 +124,8 @@ function image(elm, data){
     image1Elm.src=data.bandas[0].imagen;
     image2Elm.src=data.bandas[0].imagen;
   } else if(elm=='label'){
-
+    image1Elm.src=data.discograficas[0].imagen;
+    image2Elm.src=data.discograficas[0].imagen;
   } else if(elm=='musician'){
     image1Elm.src=data.musicos[0].imagen;
     image2Elm.src=data.musicos[0].imagen;
@@ -144,7 +153,10 @@ function mainInfo(elm, data){
           <li><strong>Etapas</strong>: ${getStages(data.etapas_bandas)}</li>
           <li><strong>Temas de Letra</strong>: ${getBandLyricThemes(data.temas_letra_bandas)}</li>`;
   } else if(elm=='label'){
-
+    let label = data.discograficas[0];
+    txt= `<li><strong>País</strong>: ${(label.pais)?label.pais:"Desconocido"}</li>
+          <li><strong>Dirección</strong>: ${(label.direccion)?label.direccion:"Desconocido"}</li>
+          <li><strong>Estatus</strong>: ${label.estatus}</li>`;
   } else if(elm=='musician'){
     let musician = data.musicos[0];
     txt= `<li><strong>País</strong>: ${(musician.pais)?musician.pais:"Desconocido"}</li>
@@ -156,13 +168,13 @@ function mainInfo(elm, data){
   mainInfoElm.innerHTML=txt;
 }
 
-function recording(elm, data){
+async function recording(elm, data){
   console.log("recording",elm,data);
   let labels = data.discograficas, studios = data.estudios_grabacion, txt="";
   if(elm=='album' || elm=='band'){
     txt=`<ul class="mb-0"><li><strong>Discográficas</strong>: <ul>`;
     for(let label of labels){
-      txt+=`<li><i class="bx bx-chevron-right"></i><a href="element=label$label=${label.discografica}">${label.discografica}</a> - ${(label.direccion)?label.direccion+", ":""}${(label.pais)?label.pais:"Origen Desconocido"}</li>`;
+      txt+=`<li><i class="bx bx-chevron-right"></i><a href="visor.html?element=label&label=${label.discografica}">${label.discografica}</a> - ${(label.direccion)?label.direccion+", ":""}${(label.pais)?label.pais:"Origen Desconocido"}</li>`;
     }
     txt+= `</ul></li><li><strong>Estudios de Grabación</strong>: <ul>`;
     for(let studio of studios){
@@ -179,6 +191,16 @@ function recording(elm, data){
     txt+= `<ul class="mb-0"><li><strong>Roles de Grabación</strong>: <ul>`;
     for(let rol of roles){
       txt+=`<li><i class="bx bx-chevron-right"></i>${rol}</li>`;
+    }
+    txt+= `</ul></li></ul>`;
+    recordingElm.innerHTML=txt;
+    recordingElm.parentElement.parentElement.classList.remove("d-none");
+  } else if(elm=='label'){
+    let studios = await list('estudios_grabacion',true,['nombreAlbum',data.albumes.map(a=>a.album).join('|')]);
+    studios = studios.response;
+    txt=`<ul class="mb-0"><li><strong>Estudios de Grabación</strong>: <ul>`;
+    for(let studio of studios){
+      txt+=`<li><i class="bx bx-chevron-right"></i>${studio.estudio} - ${(studio.direccion)?studio.direccion+", ":""}${(studio.pais)?studio.pais:"Origen Desconocido"}</li>`;
     }
     txt+= `</ul></li></ul>`;
     recordingElm.innerHTML=txt;
@@ -207,11 +229,15 @@ function links(elm, data){
   if(elm=='album'){
     txt+=`<li><strong>Amazon</strong>: <a href="${data.albumes[0].linkAmazon}" target="_blank">${data.albumes[0].album}</a></li>`;
   } else if(elm=='band'){
-    txt+=`<li><strong>Página Web</strong>: <a href="${data.bandas[0].linkWeb}" target="_blank">${data.bandas[0].banda}</a></li>
-          <li><strong>Amazon</strong>: <a href="${data.bandas[0].linkAmazon}" target="_blank">${data.bandas[0].banda}</a></li>`;
-  } else if(elm=='label' || elm=='musician'){
+    if(data.bandas[0].linkWeb) txt+=`<li><strong>Página Web</strong>: <a href="${data.bandas[0].linkWeb}" target="_blank">${data.bandas[0].banda}</a></li>`;
+    else txt+=`<li><strong>Página Web</strong>: No Disponible</li>`;
+  } else if(elm=='label'){
+    if(data.discograficas[0].linkWeb) txt+=`<li><strong>Página Web</strong>: <a href="${data.discograficas[0].linkWeb}" target="_blank">${data.discograficas[0].discografica}</a></li>`;
+    else txt+=`<li><strong>Página Web</strong>: No Disponible</li>`;
+  } else if(elm=='musician'){
     for(let band of data.bandas){
-      txt+=`<li><strong>Página Web</strong>: <a href="${band.linkWeb}" target="_blank">${band.banda}</a></li>`;
+      if(data.bandas[0].linkWeb) txt+=`<li><strong>Página Web</strong>: <a href="${band.linkWeb}" target="_blank">${band.banda}</a></li>`;
+      else txt+=`<li><strong>Página Web</strong>: No Disponible</li>`;
     }
   }
   linksElm.innerHTML=txt;
@@ -222,8 +248,8 @@ function topAlbums(elm, data){
   let mesEsp = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
   let txt = "";
   let albums = data.albumes;
-  if(elm=='band' || elm=='musician'){
-    for(let album of albums){
+  if(elm=='band'){
+    for(let album of albums.filter(a=>a.tipo!=null)){
       txt+=`
       <div class="col-12 col-md-6 col-xxl-4 py-2 elmCard">
         <div class="row">
@@ -238,8 +264,20 @@ function topAlbums(elm, data){
     }
     topAlbumsElm.innerHTML=txt;
     topAlbumsElm.parentElement.parentElement.classList.remove("d-none");
-  } else if(elm=='label'){
-
+  } else if(elm=='musician' || elm=='label'){
+    for(let album of albums.filter(a=>a.tipo!=null)){
+      txt+=`
+      <div class="col-12 col-md-6 col-xxl-4 py-2 elmCard">
+        <div class="row">
+          <div class="col-auto pe-0"><img src="${album.imagen}" class="rounded-1 shadow" alt="Imagen del álbum ${album.album}"></div>
+          <div class="col-auto ps-2 elmCardText">
+            <p class="mb-0 fs-6"><a href="visor.html?element=album&band=${album.banda}&album=${album.album}">${(data.bandas.length>1)?album.banda+' - ':''}${album.album}</a></p>
+            <hr class="mt-1 mb-2">
+            <p class="mb-0">${(album.dia)?album.dia+" de ":""}${(album.mes)?mesEsp[parseInt(album.mes)]+" de ":""}${(album.anio)?album.anio:""}</p>
+          </div>
+        </div>
+      </div>`;
+    }
     topAlbumsElm.innerHTML=txt;
     topAlbumsElm.parentElement.parentElement.classList.remove("d-none");
   }
@@ -247,9 +285,21 @@ function topAlbums(elm, data){
 
 function topBands(elm, data){
   console.log("topBands",elm,data);
-  let bands = data.bandas, txt="";
+  let txt="", bands = data.bandas;
   if(elm=='label'){
-
+    for(let band of bands){
+      txt+=`
+      <div class="col-12 col-md-6 col-xxl-4 py-2 elmCard">
+        <div class="row">
+          <div class="col-auto pe-0"><img src="${band.imagen}" class="rounded-1 shadow" alt="Imagen de la banda ${band.banda}"></div>
+          <div class="col-auto ps-2 elmCardText">
+            <p class="mb-0 fs-6"><a href="visor.html?element=band&band=${band.banda}">${band.banda}</a></p>
+            <hr class="mt-1 mb-2">
+            <p class="mb-0">${(band.origen)?band.origen+', ':''}${(band.pais)?band.pais:''}</p>
+          </div>
+        </div>
+      </div>`;
+    }
     topBandsElm.innerHTML=txt;
     topBandsElm.parentElement.parentElement.classList.remove("d-none");
   } else if(elm=='musician'){
@@ -261,7 +311,7 @@ function topBands(elm, data){
           <div class="col-auto ps-2 elmCardText">
             <p class="mb-0 fs-6"><a href="visor.html?element=band&band=${band.banda}">${band.banda}</a></p>
             <hr class="mt-1 mb-2">
-            <p class="mb-0">${getStages(data.musicos_bandas.filter(m=>m.banda==band.banda))}</p>
+            <p class="mb-0">${getStages(data.musicos_bandas.filter(m=>m.musico==data.musicos[0].musico))}</p>
           </div>
         </div>
       </div>`;
@@ -320,11 +370,24 @@ function topMusicians(elm, data){
     }
     topMusiciansElm.innerHTML=txt;
     topMusiciansElm.parentElement.parentElement.classList.remove("d-none");
-  } else if(elm=='label'){
-
+  } /* else if(elm=='label'){
+    let musiciansList = data.musicos;
+    for(let musician of musiciansList){
+      txt+=`
+      <div class="col-12 col-md-6 col-xxl-4 py-2 elmCard">
+        <div class="row">
+          <div class="col-auto pe-0"><img src="${musician.imagen}" class="rounded-1 shadow" alt="Imagen del músico ${musician.musico}"></div>
+          <div class="col-auto ps-2 elmCardText">
+            <p class="mb-0 fs-6"><a href="visor.html?element=musician&musician=${musician.musico}">${musician.musico}</a></p>
+            <hr class="mt-1 mb-2">
+            <p class="mb-0">${(musician.origen)?musician.origen+', ':''}${(musician.pais)?musician.pais:'Origen Desconocido'}</p>
+          </div>
+        </div>
+      </div>`;
+    }
     topMusiciansElm.innerHTML=txt;
     topMusiciansElm.parentElement.parentElement.classList.remove("d-none");
-  }
+  } */
 }
 
 function topGenres(elm, data){
@@ -374,8 +437,8 @@ function topSongs(elm, data){
     txt+=`<div class="mt-4">${data.albumes[0].linkSpotify}</div>`;
     topSongsElm.innerHTML=txt;
     topSongsElm.parentElement.parentElement.parentElement.classList.remove("d-none");
-  } else if(elm=='band' || elm=='musician'){
-    for(let song of data.canciones_albumes){
+  } else if(elm=='band'){
+    for(let song of data.canciones_albumes.filter(b=>data.bandas.map(b=>b.banda).includes(b.banda))){
       txt+=`<li>
               <i class="bi bi-star${(song.estrellas>=1)?"-fill":""}"></i>
               <i class="bi bi-star${(song.estrellas>=2)?"-fill":""}"></i>
@@ -385,7 +448,15 @@ function topSongs(elm, data){
     }
     topSongsElm.innerHTML=txt;
     topSongsElm.parentElement.parentElement.parentElement.classList.remove("d-none");
-  } else if(elm=='label'){
+  } else if(elm=='musician' || elm=='label'){
+    for(let song of data.canciones_albumes.filter(b=>data.bandas.map(b=>b.banda).includes(b.banda))){
+      txt+=`<li>
+              <i class="bi bi-star${(song.estrellas>=1)?"-fill":""}"></i>
+              <i class="bi bi-star${(song.estrellas>=2)?"-fill":""}"></i>
+              <i class="bi bi-star${(song.estrellas==3)?"-fill":""}"></i>
+              ${song.cancion}${(data.bandas.length>1)?" ("+song.banda+")":""}
+            </li>`;
+    }
     topSongsElm.innerHTML=txt;
     topSongsElm.parentElement.parentElement.parentElement.classList.remove("d-none");
   }
